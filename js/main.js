b6e3f4c81ad0a36e224828be03a490ddcbca61e0
Hebimalu0000 <hebimalu@outlook.com>
2025-02-16 14:58:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 const run = document.getElementById('run');
 const workspace = Blockly.inject("blocklyDiv", {
-  toolbox: document.getElementById("toolbox")
+  toolbox: document.getElementById("toolbox"),
+  grid: {
+    spacing: 20,
+    length: 1,
+    colour: '#ccc',
+    snap: true
+  },
+  move:{ 
+    scrollbars: {
+      horizontal: true,
+      vertical: true
+    },
+    drag: true,
+    wheel: false
+  },
+  zoom: {
+    controls: true,
+    wheel: true,
+    startScale: 1.0,
+    maxScale: 3,
+    minScale: 0.3,
+    scaleSpeed: 1.2,
+    pinch: true
+  },
+  trashcan: true
 });
 
 run.addEventListener("click", updateBlocks);
